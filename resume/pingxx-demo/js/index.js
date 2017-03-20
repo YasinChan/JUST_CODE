8788d3aa62a554881099d098a4bbf684532b3034
yasinchen <yasinchan2016@gmail.com>
2017-03-20 14:30:08 +0800

    update ping++demo

---
+++ resume/pingxx-demo/js/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yaoyao on 2017/3/20.
+ */
+
+$('.btn').on('click', function () {
+    $('.fixed-tab').animate({
+        right: 0
+    })
+})
+
+$('.x').on('click', function () {
+    $('.fixed-tab').animate({
+        right: -420
+    })
+})
+
+
+
+
+
